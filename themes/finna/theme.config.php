b182cbf09045796febd8f48c9726561301feea15
jhmesso <jyrki@messo.fi>
2016-09-28 11:06:50 +0300

    Finna Street Search

--- themes/finna/theme.config.php
+++ themes/finna/theme.config.php
@@ -42,7 +42,6 @@ return array(
             'proxyurl' => 'Finna\View\Helper\Root\Factory::getProxyUrl',
         ),
         'invokables' => array(
-	    'body' => 'Finna\View\Helper\Root\Body',
             'checkboxFacetCounts' =>
                 'Finna\View\Helper\Root\CheckboxFacetCounts',
             'resultfeed' => 'Finna\View\Helper\Root\ResultFeed',

4009e2b231c5db4af56ac5bad3a4b5c796d7a26f
jhmesso <jyrki@messo.fi>
2016-10-11 09:29:55 +0300

    [ALLI-4670] Minor change to regexps

--- themes/finna/js/finna-mylist.js
+++ themes/finna/js/finna-mylist.js
@@ -380,11 +380,11 @@ finna.myList = (function() {
 		})
 		    .done(function() {
 			if (window.location.href.match(/sort=\w+$/)) {
-			    window.location.href = window.location.href.replace(/sort=\w+?$/,"sort=own_ordering");
+			    window.location.href = window.location.href.replace(/sort=\w+?$/,"");
 			} else if (window.location.href.match(/sort=\w+?&(.*)$/)) {
-			    window.location.href = window.location.href.replace(/sort=\w+?&(.*)$/,"sort=own_ordering&$1");
+			    window.location.href = window.location.href.replace(/sort=\w+?&(.*)$/,"$1");
 			} else {
-                          window.location.href += "&sort=own_ordering";
+			    window.location.reload();
 			}
 		    })
 		    .fail(function() {
